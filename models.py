b65e5810c244b7cb9e4d7421c0574d9c4f0be4e6
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2022-11-18 08:36:37 +0100

    Add files via upload

--- models.py
+++ models.py
@@ -1,27 +1,6 @@
 import torch
 import torch.nn as nn
 
-class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(LSTMModel, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
-    
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-
-        if torch.cuda.is_available():
-            h0 = h0.cuda()
-            c0 = c0.cuda()
-        
-        out, _ = self.lstm(x, (h0, c0))
-
-        out = self.fc(out[:, -1, :])
-        return out
-
 
 class RNNModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
@@ -49,21 +28,26 @@ class CNNModel(nn.Module):
         super().__init__()
 
         self.features = nn.Sequential(
-            nn.Conv1d(in_channels=2, out_channels=25, kernel_size=1),
+            nn.Conv1d(in_channels=2, out_channels=25, kernel_size=3),
             nn.BatchNorm1d(25),
-            nn.ReLU()
+            nn.ReLU(),
+
+#             nn.Conv1d(in_channels=25, out_channels=25, kernel_size=1),
+#             nn.BatchNorm1d(25),
+#             nn.ReLU()
+
         )
 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
-            nn.Linear(7500, 512),
+            nn.Linear(7450, 512),
             nn.ReLU(),
             
             nn.Dropout(p=0.5),            
             nn.Linear(512, 256),
             nn.ReLU(),
             
-            nn.Linear(256, output),
+            nn.Linear(256, output)
         )
         
     def forward(self, x):
